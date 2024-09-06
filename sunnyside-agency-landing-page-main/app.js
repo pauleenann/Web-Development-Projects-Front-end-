4f4a72997a40d8bd68f84963836355f4e25e48c5
Pau <141435859+pauleenann@users.noreply.github.com>
2024-09-06 10:12:54 +0800

    initial commit

---
+++ sunnyside-agency-landing-page-main/app.js
@@ -0,0 +1,8 @@
+let navToggle = document.getElementById('nav-toggle');
+let links = document.getElementById('links')
+
+
+navToggle.addEventListener('click', ()=>{
+    links.classList.toggle('show-links')
+    
+})
